23b1ef24def2794e33c7b29fe46c0d5b68ca319b
anzha12f <anzha12f@gmail.com>
2024-09-16 05:39:09 +0000

    commit all

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -0,0 +1,8 @@
+package com.example.explorecalijpa.repo;
+
+
+import com.example.explorecalijpa.model.TourPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+public class TourPackageRepository extends JpaRepository<TourPackage, String> {
+  
+}
